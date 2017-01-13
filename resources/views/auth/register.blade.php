09375c09de77e0f8878599a2741b17a5f8a61d83
wdarnellg <dstroketennis@gmail.com>
2017-01-13 13:57:27 +0000

    removed scripts to registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -93,7 +93,7 @@
                                 </button>
                             </div>
                         </div>
-                        <div class="g-recaptcha" data-sitekey="6LetuBEUAAAAAEknuM1hwnMHlygEbYiyS1HSX_Fs"></div>
+                        
                     </form>
                 </div>
             </div>
